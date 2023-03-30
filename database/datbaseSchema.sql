98a03b4dcbb833f00e8fa562c5171553e365a406
Poitier Stringer <stringerpoitier@gmail.com>
2023-03-30 12:30:37 -0700

    switch key columns to varchar

--- database/datbaseSchema.sql
+++ database/datbaseSchema.sql
@@ -1,56 +1,28 @@
-/*
 CREATE TABLE patient (
-	first_name TEXT NOT NULL,
- 	last_name TEXT NOT NULL,
-	birth_date data_type TEXT NOT NULL,
-	mrn TEXT NOT NULL, 
-	location TEXT NOT NULL
-) 
-
-CREATE TABLE appointment (
-	appointment_id TEXT NOT NULL,
-	mrn TEXT NOT NULL, 
-	npi TEXT NOT NULL, 
-	appointment_time TEXT NOT NULL
-)
-
-CREATE TABLE location (
-	location_id TEXT NOT NULL, 
-	location_name TEXT NOT NULL
-) 
-
-CREATE TABLE physician (
-	npi TEXT NOT NULL, 
-	first_name TEXT NOT NULL,
-	last_name TEXT NOT NULL
-) 
-*/
-
-CREATE TABLE patient (
-	mrn UUID PRIMARY KEY,
+	mrn VARCHAR(36) PRIMARY KEY,
 	first_name VARCHAR(15) NOT NULL,
 	last_name VARCHAR(15) NOT NULL,
 	birth_date DATE NOT NULL,
-	location_id UUID NOT NULL,
+	location_id VARCHAR(36) NOT NULL,
 	FOREIGN KEY (location_id) REFERENCES location(location_id)
 )
 
 CREATE TABLE appointment (
-	appointment_id UUID PRIMARY KEY,
-	mrn UUID NOT NULL,
-	npi UUID NOT NULL,
+	appointment_id VARCHAR(36) PRIMARY KEY,
+	mrn VARCHAR(36) NOT NULL,
+	npi VARCHAR(36) NOT NULL,
 	appointment_time DATETIME NOT NULL,
 	FOREIGN KEY (mrn) REFERENCES patient(mrn),
 	FOREIGN KEY (npi) REFERENCES physician(npi)
 )
 
 CREATE TABLE location (
-	location_id UUID PRIMARY KEY,
+	location_id VARCHAR(36) PRIMARY KEY,
 	location_name VARCHAR(50) NOT NULL
 )
 
 CREATE TABLE physician (
-	npi UUID PRIMARY KEY,
+	npi VARCHAR(36) PRIMARY KEY,
 	first_name VARCHAR(15) NOT NULL,
 	last_name VARCHAR(15) NOT NULL
 )
