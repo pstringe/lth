3ba1dca147334c2429049ac6321e62f853953d8f
Poitier Stringer <stringerpoitier@gmail.com>
2023-03-31 18:16:53 -0700

    set up typescript

--- database/scripts/seed.js
+++ database/scripts/seed.js
@@ -11,46 +11,12 @@ const physicians = require('../physicians.json');
 const appointments = require('../appointments.json');
 const locations = require('../locations.json');
 
-console.log(db);
-//console.log('Data imported', patients, physicians, appointments, locations);
-
 /*
-db schema
-
-CREATE TABLE patient (
-	mrn BLOB(16) PRIMARY KEY,
-	first_name TEXT NOT NULL,
-	last_name TEXT NOT NULL,
-	birth_date DATE NOT NULL,
-	location_id BLOB(16) NOT NULL,
-	FOREIGN KEY (location_id) REFERENCES location(location_id)
-);
-
-
-CREATE TABLE appointment (
-	appointment_id BLOB(16) PRIMARY KEY,
-	mrn BLOB(16) NOT NULL,
-	npi BLOB(16) NOT NULL,
-	appointment_time DATETIME NOT NULL,
-	FOREIGN KEY (mrn) REFERENCES patient(mrn),
-	FOREIGN KEY (npi) REFERENCES physician(npi)
-);
-
-
-CREATE TABLE location (
-	location_id BLOB(16) PRIMARY KEY,
-	location_name TEXT NOT NULL
-);
-
-
-CREATE TABLE physician (
-	npi BLOB(16) PRIMARY KEY,
-	first_name TEXT NOT NULL,
-	last_name TEXT NOT NULL
-);
+** Utility function for converting UUID to Blob type for more efficient storage
 */
-console.log({patients});
+
 const uuidToBlob = (uuid) => {
+    if (!uuid) return null;
     const hex = uuid.replace(/-/g, '');
     const bytes = new Uint8Array(hex.match(/.{1,2}/g).map(byte => parseInt(byte, 16)));
     return bytes;
@@ -66,6 +32,54 @@ db.serialize(() => {
             patient.lastName, 
             patient.birthdate, 
             uuidToBlob(patient.location)
+        ], function(err) {
+            if (err) {
+                return console.log(err.message);
+            }
+            console.log(`A row has been inserted with rowid ${this.lastID}`);
+        });
+    });
+});
+
+db.serialize(() => {
+    const sql = `INSERT INTO physician (npi, first_name, last_name) VALUES (?, ?, ?)`;
+    physicians.forEach(physician => {
+        db.run(sql, [
+            uuidToBlob(physician.npi),
+            physician.firstName,
+            physician.lastName
+        ], function(err) {
+            if (err) {
+                return console.log(err.message);
+            }
+            console.log(`A row has been inserted with rowid ${this.lastID}`);
+        });
+    });
+});
+
+db.serialize(() => {
+    const sql = `INSERT INTO location (location_id, location_name) VALUES (?, ?)`;
+    locations.forEach(location => {
+        db.run(sql, [
+            uuidToBlob(location.locationId),
+            location.locationName
+        ], function(err) {
+            if (err) {
+                return console.log(err.message);
+            }
+            console.log(`A row has been inserted with rowid ${this.lastID}`);
+        });
+    });
+});
+
+db.serialize(() => {
+    const sql = `INSERT INTO appointment (appointment_id, mrn, npi, appointment_time) VALUES (?, ?, ?, ?)`;
+    appointments.forEach(appointment => {
+        db.run(sql, [
+            uuidToBlob(appointment.appointmentId),
+            uuidToBlob(appointment.mrn),
+            uuidToBlob(appointment.npi),
+            appointment.time
         ], function(err) {
             if (err) {
                 return console.log(err.message);
